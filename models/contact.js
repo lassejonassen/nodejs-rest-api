217986c38892b1b0c71bba2ee1cfef93899727a4
Lasse Jonassen <lassemoellerjonassen@gmail.com>
2021-02-16 15:17:38 +0000

    Started on homepage

--- models/contact.js
+++ models/contact.js
@@ -10,7 +10,7 @@ const contactSchema = new mongoose.Schema({
         required: true
     },
     mobile: {
-        type: number,
+        type: Number,
         required: true
     },
     notes: {
